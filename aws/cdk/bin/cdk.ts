46c964ae00a7a91aa434cc6bbba83aabd61fad27
sarangan2209 <sarangan@tecofize.com>
2025-05-19 14:12:59 +0530

    AWS Transfer Family

--- aws/cdk/bin/cdk.ts
+++ aws/cdk/bin/cdk.ts
@@ -2,6 +2,7 @@
 import * as cdk from 'aws-cdk-lib';
 import { NetworkStack } from '../lib/network-stack';
 import { FtpEc2Stack } from '../lib/ftp-ec2-stack';
+import { SftpTransferStack } from '../lib/sftp-transfer-stack';
 
 
 
@@ -16,12 +17,21 @@ const app = new cdk.App();
 // }, 'NetworkStack');
 
 
-const AWS_ACCOUNT_ID = ''; 
+const AWS_ACCOUNT_ID = '369714029605'; 
 const AWS_REGION = 'us-east-1';
 
-new FtpEc2Stack(app, 'FtpEc2Stack', {
+// new FtpEc2Stack(app, 'FtpEc2Stack', {
+//   env: {
+//     account: AWS_ACCOUNT_ID,
+//     region: AWS_REGION,
+//   },
+// });
+
+new SftpTransferStack(app, 'SftpTransferStack', {
   env: {
     account: AWS_ACCOUNT_ID,
     region: AWS_REGION,
   },
+  userName: 'sftpuser',
+  publicKeyPath: '/home/sarangan/Downloads/keypairs/sftp.pub',
 });
